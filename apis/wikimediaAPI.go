6b37e589239e4a98bae9afccefebc3128de880b3
user.mail <cristian.vidal@autofact.cl>
2025-01-08 17:56:16 -0300

    se adiciona api wikimedia

---
+++ apis/wikimediaAPI.go
@@ -0,0 +1,259 @@
+package apis
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+)
+
+// --------------- LISTADO DE IMAGENES ---------------------------------------
+
+// SearchResult representa cada resultado individual en la sección "search".
+type SearchResult struct {
+	NS        int       `json:"ns"`
+	Title     string    `json:"title"`
+	PageID    int       `json:"pageid"`
+	Size      int       `json:"size"`
+	WordCount int       `json:"wordcount"`
+	Snippet   string    `json:"snippet"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// Query contiene la información de la consulta y la lista de resultados.
+type Query struct {
+	SearchInfo struct {
+		TotalHits int `json:"totalhits"`
+	} `json:"searchinfo"`
+	Search []SearchResult `json:"search"`
+}
+
+// Continue contiene los datos para la paginación.
+type Continue struct {
+	SrOffset int    `json:"sroffset"`
+	Continue string `json:"continue"`
+}
+
+// APIResponse representa la estructura completa del JSON de respuesta.
+type WikimediaAPIResponse struct {
+	BatchComplete string   `json:"batchcomplete"`
+	Continue      Continue `json:"continue"`
+	Query         Query    `json:"query"`
+}
+
+// --------------- DETALLE PARA UNA IMAGEN ---------------------------------------
+// ImageInfo contiene detalles específicos de la imagen.
+type ImageInfo struct {
+	User                string                 `json:"user"`
+	URL                 string                 `json:"url"`
+	DescriptionURL      string                 `json:"descriptionurl"`
+	DescriptionShortURL string                 `json:"descriptionshorturl"`
+	ExtMetadata         map[string]interface{} `json:"extmetadata"`
+}
+
+// Page contiene detalles de la página relacionada con la imagen.
+type PageDetail struct {
+	NS                  int         `json:"ns"`
+	Title               string      `json:"title"`
+	Missing             string      `json:"missing"`
+	Known               string      `json:"known"`
+	ImageRepository     string      `json:"imagerepository"`
+	ImageInfo           []ImageInfo `json:"imageinfo"`
+	LicenseShortName    string      `json:"LicenseShortName"`
+	UsageTerms          string      `json:"UsageTerms"`
+	AttributionRequired bool        `json:"AttributionRequired"`
+	LicenseUrl          string      `json:"LicenseUrl"`
+	Copyrighted         bool        `json:"Copyrighted"`
+	Restrictions        string      `json:"Restrictions"`
+	License             string      `json:"License"`
+}
+
+// Query contiene los resultados de la consulta.
+type QueryDetail struct {
+	Normalized []struct {
+		From string `json:"from"`
+		To   string `json:"to"`
+	} `json:"normalized"`
+	Pages map[string]PageDetail `json:"pages"`
+}
+
+// Continue indica los datos para la paginación o continuación.
+type ContinueDetail struct {
+	IiStart  string `json:"iistart"`
+	Continue string `json:"continue"`
+}
+
+type WikimediaDetailAPIResponse struct {
+	Continue ContinueDetail `json:"continue"`
+	Query    QueryDetail    `json:"query"`
+}
+
+// Estructura para mapear el JSON parcialmente
+type ResponseImageData struct {
+	Query QueryImageData `json:"query"`
+}
+
+// QueryData contiene la información principal de la consulta
+type QueryImageData struct {
+	Pages map[string]PageImageData `json:"pages"`
+}
+
+// PageData representa una página de resultados
+type PageImageData struct {
+	ImageInfo []ImageInfoData `json:"imageinfo"`
+}
+
+// ImageInfoData representa la información de una imagen
+type ImageInfoData struct {
+	URL            string          `json:"url"`            // URL directa de la imagen
+	DescriptionURL string          `json:"descriptionurl"` // URL a la descripción de la imagen
+	ExtMetadata    ExtMetadataData `json:"extmetadata"`    // Metadatos adicionales
+}
+
+// ExtMetadataData contiene los metadatos de la imagen
+type ExtMetadataData struct {
+	ObjectName       MetadataValue `json:"ObjectName"`       // Nombre del objeto
+	Artist           MetadataValue `json:"Artist"`           // Artista
+	LicenseShortName MetadataValue `json:"LicenseShortName"` // Nombre abreviado de la licencia
+	LicenseUrl       MetadataValue `json:"LicenseUrl"`       // URL de la licencia
+}
+
+// MetadataValue encapsula valores comunes en los metadatos
+type MetadataValue struct {
+	Value string `json:"value"` // Valor del metadato
+}
+
+type GetBirdImageDetailResult struct {
+	URL              string        `json:"url"`              // URL directa de la imagen
+	DescriptionURL   string        `json:"descriptionurl"`   // URL a la descripción de la imagen
+	ObjectName       MetadataValue `json:"ObjectName"`       // Nombre del objeto
+	Artist           string        `json:"Artist"`           // Artista
+	LicenseShortName string        `json:"LicenseShortName"` // Nombre abreviado de la licencia
+	LicenseUrl       string        `json:"LicenseUrl"`       // URL de la licencia
+}
+
+// callExternalAPI simula una llamada a una API externa.
+func GetBirdImageDetail(title string, resultChan chan<- GetBirdImageDetailResult, wg *sync.WaitGroup) {
+	defer wg.Done() // Asegura que el contador se disminuye una vez termine esta goroutine.
+
+	url := fmt.Sprintf("https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata|user|url&titles=File:%s&format=json", ReplaceSpacesWithUnderscore(title))
+
+	var apiResponse ResponseImageData
+
+	err := fetchWithRetry(url, &apiResponse, true)
+	if err != nil {
+		return
+	}
+
+	// Lista de nombres de licencias gratuitas permitidas
+	freeLicenses := []string{
+		"CC BY 2.0",
+		"CC BY-SA 2.0",
+		"CC BY 3.0",
+		"CC BY-SA 3.0",
+		"CC BY 4.0",
+		"CC BY-SA 4.0",
+	}
+
+	// Procesar los datos del JSON
+	for _, page := range apiResponse.Query.Pages {
+		for _, imageInfo := range page.ImageInfo {
+			license := imageInfo.ExtMetadata.LicenseShortName.Value
+
+			// Validar si la licencia es gratuita
+			isFreeLicense := false
+			for _, freeLicense := range freeLicenses {
+				if license == freeLicense {
+					isFreeLicense = true
+					break
+				}
+			}
+
+			// Si la licencia no es gratuita, ignorar este registro
+			if !isFreeLicense {
+				continue
+			}
+
+			// Extraer y crear el objeto
+			artist := extractArtist(imageInfo.ExtMetadata.Artist.Value)
+			image := GetBirdImageDetailResult{
+				ObjectName:       imageInfo.ExtMetadata.ObjectName,
+				URL:              imageInfo.URL,
+				DescriptionURL:   imageInfo.DescriptionURL,
+				Artist:           artist,
+				LicenseShortName: license,
+				LicenseUrl:       imageInfo.ExtMetadata.LicenseUrl.Value,
+			}
+
+			resultChan <- image // Enviar resultado al canal
+		}
+	}
+}
+
+// Llamada a la API de Wikipedia
+func GetAllBirdImageDetails(nameBird string) ([]GetBirdImageDetailResult, error) {
+	// Comprobar si el nombre científico está en el mapa y usar el nombre correcto
+	if correctName, exists := latinToCommonName[nameBird]; exists {
+		nameBird = correctName
+	}
+
+	// Crear un arreglo para almacenar todos los resultados
+	var allImages []GetBirdImageDetailResult
+
+	url := fmt.Sprintf("https://commons.wikimedia.org/w/api.php?action=query&format=json&list=search&srsearch=%s&srnamespace=6&utf8=&srlimit=10",
+		ReplaceSpacesWithUnderscore(nameBird))
+
+	var apiResponse WikimediaAPIResponse
+
+	err := fetchWithRetry(url, &apiResponse, true)
+	if err != nil {
+		return []GetBirdImageDetailResult{}, err
+	}
+
+	fmt.Printf("Resultados totales: %d\n", apiResponse.Query.SearchInfo.TotalHits)
+
+	var wg sync.WaitGroup
+	resultChan := make(chan GetBirdImageDetailResult, 100) // Canal para resultados
+
+	// Iterar sobre cada título
+	for _, result := range apiResponse.Query.Search {
+		// Eliminamos "File:" del título
+		sanitizedTitle := removePrefix(result.Title, "File:")
+		fmt.Printf("Título: %s\n", sanitizedTitle)
+
+		wg.Add(1) // Incrementar el contador de goroutines
+
+		go GetBirdImageDetail(sanitizedTitle, resultChan, &wg)
+	}
+
+	// Esperar a que todas las goroutines terminen
+	go func() {
+		wg.Wait()
+		close(resultChan) // Cerrar el canal cuando todo haya terminado
+	}()
+
+	// Recoger los resultados del canal
+	for image := range resultChan {
+		allImages = append(allImages, image)
+	}
+
+	return allImages, nil
+}
+
+// removePrefix elimina un prefijo dado de un string.
+func removePrefix(text, prefix string) string {
+	if len(text) >= len(prefix) && text[:len(prefix)] == prefix {
+		return text[len(prefix):]
+	}
+	return text
+}
+
+func extractArtist(value string) string {
+	// Extraer el contenido dentro de las etiquetas <a>
+	start := strings.Index(value, ">") + 1
+	end := strings.LastIndex(value, "<")
+	if start > 0 && end > start {
+		return value[start:end]
+	}
+	return ""
+}
--- services/process.go
+++ services/process.go
@@ -11,14 +11,15 @@ import (
 type Result struct {
 	BirdDetail  *apis.ResultBirdDetail // Detalles de la ave obtenidos de la API
 	BirdExtract string                 // Extracto de Wikipedia para la ave
-	Error       error                  // Posible error durante el proceso
+	Images      []apis.GetBirdImageDetailResult
+	Error       error // Posible error durante el proceso
 }
 
 // ProcessRecord procesa un registro llamando a las APIs X2 y X3
 // Esta función toma un registro de tipo 'Bird', llama a las APIs para obtener información
 // adicional sobre la ave y luego simula almacenar estos datos en una base de datos.
 func ProcessRecord(record models.Bird) error {
-	var bufferCount = 2
+	var bufferCount = 3
 
 	// Canal único para recibir los resultados: tanto el detalle de la ave como el extracto de Wikipedia.
 	resultChannel := make(chan Result, bufferCount) // Canal con buffer de 2 para recibir ambos resultados
@@ -54,9 +55,22 @@ func ProcessRecord(record models.Bird) error {
 		}
 	}()
 
+	// Goroutine para obtener las imágenes asociadas
+	go func() {
+		// Títulos de imágenes asociados a la ave
+		birdImages, err := apis.GetAllBirdImageDetails(record.Name.Latin)
+		if err != nil {
+			resultChannel <- Result{Error: fmt.Errorf("Error obteniendo imágenes de la ave %s: %v", record.Name.Latin, err)}
+			return
+		}
+		// Agregamos las imágenes al resultado
+		resultChannel <- Result{Images: birdImages}
+	}()
+
 	// Variables para almacenar los resultados recibidos del canal
 	var birdDetail *apis.ResultBirdDetail
 	var birdExtract string
+	var birdImages []apis.GetBirdImageDetailResult
 	var errorResult error
 
 	// Recoger los resultados del canal
@@ -64,6 +78,7 @@ func ProcessRecord(record models.Bird) error {
 	for i := 0; i < bufferCount; i++ {
 		// Se recibe un resultado del canal
 		result := <-resultChannel
+
 		// Si el resultado contiene un error, se guarda para su posterior manejo
 		if result.Error != nil {
 			errorResult = result.Error
@@ -72,9 +87,15 @@ func ProcessRecord(record models.Bird) error {
 		if result.BirdDetail != nil {
 			birdDetail = result.BirdDetail
 		}
+
 		// Si se obtiene el extracto de Wikipedia, se guarda en la variable correspondiente
 		if result.BirdExtract != "" {
 			birdExtract = result.BirdExtract
+		}
+
+		// Si se obtiene imagenes de Wikimedia, se guarda en la variable correspondiente
+		if result.Images != nil {
+			birdImages = result.Images
 		}
 	}
 
@@ -88,7 +109,7 @@ func ProcessRecord(record models.Bird) error {
 	}
 
 	// Simulamos el almacenamiento en la base de datos con los datos obtenidos
-	simulateDBSave(record, Result{BirdDetail: birdDetail, BirdExtract: birdExtract})
+	simulateDBSave(record, Result{BirdDetail: birdDetail, BirdExtract: birdExtract, Images: birdImages})
 
 	// Retornamos nil si no hubo errores
 	return nil
@@ -143,12 +164,14 @@ func simulateDBSave(record models.Bird, result Result) {
 		fmt.Printf("Tamaño: %s\n", result.BirdDetail.Size)
 		fmt.Printf("Orden: %s\n", result.BirdDetail.Order)
 		fmt.Printf("Especie: %s\n", result.BirdDetail.Species)
-		fmt.Println("Imágenes: ", result.BirdDetail.Images)
+		fmt.Println("Imágenes (API BIRDS): ", result.BirdDetail.Images)
 		fmt.Println("Audio: ", result.BirdDetail.Audio)
 	} else {
 		// Si no se obtuvieron detalles, mostramos un mensaje indicando que no se encontraron
 		fmt.Println("Detalles de la ave no encontrados.")
 	}
+
+	fmt.Println("Imágenes (Wikimedia): ", result.Images)
 
 	// Mostramos el extracto de Wikipedia obtenido
 	fmt.Println("Extracto de Wikipedia:", result.BirdExtract)
